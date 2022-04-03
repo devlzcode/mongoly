8b7ffc0b93b4766c6d9748a7986d800ca9d8d966
Lenneth <developer.lenneth@gmail.com>
2022-04-03 11:31:18 -0700

    Added debug env variable for ensureJsonSchema

--- lib/ensureJsonSchema.ts
+++ lib/ensureJsonSchema.ts
@@ -2,6 +2,7 @@ import { isDeepStrictEqual } from "node:util";
 import type { Db } from "mongodb";
 import type { ObjectKeywords } from "./jsonSchemaTypes";
 
+const DEBUG_ENABLED = process.env.DEBUG_ENSUREJSONSCHEMA === "true";
 const BENCHMARKS_ENABLED = process.env.BENCHMARKS_ENSUREJSONSCHEMA === "true";
 
 // Ensures that the MongoDB collection has a JSON schema set to validate insertions and updates
@@ -17,13 +18,17 @@ export const ensureJsonSchema = async (
     .listCollections({ name: collectionName })
     .toArray();
   if (collections.length === 0) {
-    console.debug(`Creating collection "${collectionName}" in ${db.namespace}`);
+    if (DEBUG_ENABLED)
+      console.debug(
+        `Creating collection "${collectionName}" in ${db.namespace}`
+      );
     await db.createCollection(collectionName, { validator });
   } else {
     const collection = db.collection(collectionName);
     const options = await collection.options();
     if (!isDeepStrictEqual(options.validator, validator)) {
-      console.debug(`Updating validator for ${collection.namespace}`);
+      if (DEBUG_ENABLED)
+        console.debug(`Updating validator for ${collection.namespace}`);
       await db.command({ collMod: collectionName, validator });
     }
   }
