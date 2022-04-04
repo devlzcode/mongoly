675fbc87667de47ef9a92aee74c5ac1e90affc8e
Lenneth <developer.lenneth@gmail.com>
2022-04-04 12:18:05 -0700

    Bug fixes

---
+++ test/moreDecorators.spec.ts
@@ -0,0 +1,94 @@
+import { describe, expect, it } from "vitest";
+import { createJsonSchemaForClass, Property, Schema } from "../lib";
+
+@Schema({ additionalProperties: false })
+class Asset {
+  @Property()
+  readonly assetId: number;
+  @Property()
+  readonly name: string;
+  @Property()
+  readonly value: number;
+}
+
+const AssetJsonSchema = createJsonSchemaForClass(Asset);
+
+@Schema({ additionalProperties: false })
+class UserAsset {
+  @Property()
+  userAssetId: number;
+  @Property()
+  userId: number;
+  @Property({ isNullable: true })
+  serialNumber?: number;
+  @Property({ jsonSchema: AssetJsonSchema })
+  asset: Asset;
+}
+
+const UserAssetJsonSchema = createJsonSchemaForClass(UserAsset);
+
+@Schema({
+  mergeWith: UserAssetJsonSchema,
+  omitProperties: ["asset", "userId"],
+  renameProperties: {
+    userAssetId: "id",
+    userId: "ownerId",
+    serialNumber: "serial",
+  },
+})
+class SkinnyUserAsset {
+  id: number;
+  serial?: number;
+}
+
+const SkinnyUserAssetSchema = createJsonSchemaForClass(SkinnyUserAsset);
+
+describe("More Decorator Tests", () => {
+  it("Should create the JSON schemas", () => {
+    expect(UserAssetJsonSchema).toEqual({
+      bsonType: "object",
+      additionalProperties: false,
+      properties: {
+        userAssetId: {
+          bsonType: "number",
+        },
+        userId: {
+          bsonType: "number",
+        },
+        serialNumber: {
+          bsonType: ["null", "number"],
+        },
+        asset: {
+          bsonType: "object",
+          additionalProperties: false,
+          properties: {
+            assetId: {
+              bsonType: "number",
+            },
+            name: {
+              bsonType: "string",
+            },
+            value: {
+              bsonType: "number",
+            },
+          },
+        },
+      },
+    });
+    expect(SkinnyUserAsset).toEqual({
+      bsonType: "object",
+      additionalProperties: false,
+      properties: {
+        id: {
+          bsonType: "number",
+        },
+        ownerId: {
+          bsonType: "number",
+        },
+        serial: {
+          bsonType: ["null", "number"],
+        },
+      },
+    });
+  });
+});
